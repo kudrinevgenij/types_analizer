e923cb9f55f0f655aafcc16f9acba5a0a35b0149
Evgenij <kudrinevgenij@gmail.com>
2024-08-21 22:55:42 +0700

    frame

--- src/main/java/ru/kudrinevgeniy/Analyzer.java
+++ src/main/java/ru/kudrinevgeniy/Analyzer.java
@@ -2,7 +2,7 @@ package ru.kudrinevgeniy;
 
 import java.io.*;
 
-public class Analizer {
+public class Analyzer {
     public static void main(String[] args) {
        try (BufferedReader reader = new BufferedReader(new FileReader("data/input.txt"));
             BufferedWriter writer = new BufferedWriter(new FileWriter("data/integers.txt"))) {
